ffe86a36be2237586d49ee3d317da304d6effb77
curtis johnson <curtisianjohnson@gmail.com>
2017-05-23 14:54:04 +0100

    server ready to deploy

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+});
+
+module.exports = app;
