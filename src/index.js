ab068f5a01418e8d727ff6539a00cf9171fc95be
curtis johnson <curtisianjohnson@gmail.com>
2017-05-23 14:55:38 +0100

    include stylesheet to index

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import './css/bulma.css';
 import './css/font-awesome.css';
+import './css/index.css';
 
+import reducer from './reducer/reducer';
 import App from './components/App';
 import FrontPage from './components/FrontPage';
 import ArticlePage from './components/ArticlePage';
-import reducer from './reducer/reducer';
+import NotFound from './components/NotFound';
+import UserPage from './components/UserPage';
 
 const store = createStore(reducer, applyMiddleware(thunk, createLogger()));
 
@@ -21,7 +24,8 @@ ReactDOM.render(<Provider store={store}>
                       <IndexRoute component={FrontPage}/>
                       <Route path=':topic' component={FrontPage} />
                       <Route path='articles/:article_id' component={ArticlePage} />
-                      {/*<Route path='users/:username' component={UserPage} />*/}
+                      <Route path='users/:username' component={UserPage} />
+                      <Route path='*' component={NotFound} />
                     </Route>
                   </Router>
                 </Provider>, document.getElementById('app'));
