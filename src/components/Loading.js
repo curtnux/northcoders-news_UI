2decbd87694e707298022fa4bbb325ccea00c418
curtis johnson <curtisianjohnson@gmail.com>
2017-05-26 16:02:16 +0100

    added loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='spinner'>
+            <p className="fa fa-spinner fa-pulse fa-3x fa-fw"></p>
+        </div>
+    );
+};
+
+export default Loading;
