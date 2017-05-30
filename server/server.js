63ac159292a146d2dc9310735c3e7bafc0a329e2
curtis johnson <curtisianjohnson@gmail.com>
2017-05-30 10:01:47 +0100

    update server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//app.use(express.static(path.resolve(__dirname, '..', 'public')));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
